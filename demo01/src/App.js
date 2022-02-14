5588774eda5bc6386159f519768daa39a4cc23e5
yaoxin <271958091@qq.com>
2022-02-14 17:52:04 +0800

    01

---
+++ demo01/src/App.js
@@ -0,0 +1,11 @@
+import React, {Components} from 'react'
+class App extends Components {
+  render () {
+    return (
+      <div>
+        HELLO WORLD
+      </div>
+    )
+  }
+}
+export default App
