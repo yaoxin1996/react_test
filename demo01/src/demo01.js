811ce56ffcc6dd28cd43283759553d0eee63f451
yaoxin <271958091@qq.com>
2022-02-15 17:32:54 +0800

    10

--- demo01/src/demo01.js
+++ demo01/src/demo01.js
@@ -1,17 +1,42 @@
 import React, {Component, Fragment} from "react";
 class Demo01 extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      inputVal: '',
+      list: ['一', '二']
+    }
+  }
   render () {
     return (
       <Fragment>
         <div>
-          <input/><button>增加</button>
+          <input value={this.state.inputVal} onChange={this.changeInput.bind(this)}/>
+          <button onClick={this.addList.bind(this)}>增加</button>
           <ul>
-            <li>一</li>
-            <li>二</li>
+            {
+              this.state.list.map(item => {
+                return <li key={item}>{item}</li>
+              })
+            }
           </ul>
         </div>
       </Fragment>
     )
   }
+  changeInput (e) {
+    console.log(this.state);
+    this.setState({
+      inputVal: e.target.value
+    })
+  }
+  addList () {
+    this.setState({
+      list: [...this.state.list, this.state.inputVal],
+      inputVal: ''
+    })
+    console.log(this.state.list);
+  }
 }
+
 export default Demo01
